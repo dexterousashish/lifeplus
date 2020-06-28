08114453404bababe289c4ae14b8bbb92267f6cc
Singh <ashish.singh2@capco.com>
2020-06-29 02:43:17 +0530

    adding modifed files

--- lifeplus/src/main/java/com/capco/lifeplus/LifeplusApplication.java
+++ lifeplus/src/main/java/com/capco/lifeplus/LifeplusApplication.java
@@ -2,23 +2,37 @@ package com.capco.lifeplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class LifeplusApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LifeplusApplication.class, args);
 	}
-
 }
 
 @RestController
 class HelloController {
-	@GetMapping("/")
-	String hello() {
-		return "Hello World";
+	
+	@PostMapping("/")
+	 public String hello() {
+		String str="{\n" + 
+				"                fulfillmentText: dataToSend,\n" + 
+				"                fulfillmentMessages: [\n" + 
+				"                  {\n" + 
+				"                    text: {\n" + 
+				"                      text: [\n" + 
+				"                        Heeeeelo What's up\n" + 
+				"                      ]\n" + 
+				"                    }\n" + 
+				"                  }\n" + 
+				"                ]\n" + 
+				"}\n" + 
+				"\n" + 
+				"";
+		return str;
 	}
 
 }
